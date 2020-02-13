3a6a0a711e267af403626e285b69527df22187d3
N. Haustein <haustein@de.ibm.com>
2020-02-13 20:29:55 +0100

    version 2.3: added bulkrecall option, added check for active control node, fixed node name with and without dots

--- launcher.sh
+++ launcher.sh
@@ -26,7 +26,7 @@
 # 
 # Name: launcher.sh
 #
-# Version 2.1
+# Version 2.3
 #
 # launcher script, checks if this is the cluster manager, performs some other checks, manages log files and launches the operation
 #
@@ -53,7 +53,9 @@
 # 11/15/19: add second argument for the service script (optional)
 # 12/11/19: implement check option with check_spectrumarchive.sh
 #           define event codes as constants and check if they are defined in the custom event file
-#
+# 02/05/19: added bulk recall
+#           add check if this is an active control node instead of manager using singleton variable set to archive
+# 02/12/20: fix problem with host name with and without dots, some streamlining (version 2.3)
 #******************************************************************************************************** 
 
 # Export the path including the linux and GPFS binaries in case this script is invoked from a schedule
@@ -65,7 +67,8 @@ export PATH=$PATH:/usr/bin:/usr/sbin:/usr/lpp/mmfs/bin
 # default file system name, if this is set and $2 is empty this name is used. If $2 is given then it is being used.
 def_fsName=""
 
-# path where the automation scripts are located
+# path where the automation scripts are located, 
+# !!! does not tolerate blanks in the path name !!!
 scriptPath="/root/silo/automation"
 
 # path and file name of the check program to be executed with the check option
@@ -84,13 +87,23 @@ verKeep=3
 # log files for this type of operation to compress - compress the $verKeep + $verComp version
 verComp=3
 
+# set default singleton to control if the node is manager or active control node with Spectrum Archive
+singleton="manager"
+
 
 # define constants
 # ----------------
 # version of the launcher program
-ver="2.1"
+ver="2.3"
+
 # path for the GPFS binaries
 gpfsPath="/usr/lpp/mmfs/bin"
+
+# eeadm command fully qualified
+eeCmd="/opt/ibm/ltfsee/bin/eeadm"
+
+# jq tool fully qualified
+JQ_TOOL="/usr/local/bin/jq"
 
 # current date
 curDate="$(date +%Y%m%d%H%M%S)"
@@ -131,14 +144,11 @@ fsName="$2"
 secArg="$3"
 
 # other global variables
-localNode=$($gpfsPath/mmlsnode -N localhost | cut -d'.' -f1)
-fsetName=""
-polFileName=""
-compName=""
+# assign the full node name, mmlsnode shows node name with dots: ltfs1.ltfs.net
+# we remove the dot later when required
+localNode=$($gpfsPath/mmlsnode -N localhost)
 errMsg=""
 errCode=""
-# temporarily set execNode to localnode
-execNode=$localNode
 logF=""
 
 #********************************************** MAIN *********************************
@@ -148,35 +158,27 @@ logF=""
 case $op in
 "backup") 
   # runs DR-Backup
-  cmd=$scriptPath"/backup.sh"
-  if [[ ! -z $secArg ]]; 
-  then
-    fsetName=$secArg
-  fi;;  
+  cmd=$scriptPath"/backup.sh $fsName $secArg";;  
 
 "migrate") 
   # runs selective migration (scheduled)
-  cmd=$scriptPath"/migrate.sh"
-  if [[ ! -z $secArg ]]; 
-  then
-    polFileName=$secArg
-  fi ;;  
-
+  cmd=$scriptPath"/migrate.sh $fsName $secArg";;
+  
 "premigrate") 
   # runs selective pre-migration (scheduled)
-  cmd=$scriptPath"/premigrate.sh"
-  if [[ ! -z $secArg ]]; 
-  then
-    polFileName=$secArg
-  fi ;;  
+  cmd=$scriptPath"/premigrate.sh $fsName $secArg";;
 
 "check")
   # runs check
-  cmd=$checkScript
-  if [[ ! -z $secArg ]]; 
-  then
-    compName=$secArg
-  fi;;
+  cmd=$checkScript" $secArg"
+  # has to run on an EE node
+  singleton="archive";;
+
+"bulkrecall")
+  # runs bulk recall, does not get any input
+  cmd=$scriptPath/bulkRecall.sh
+  # for bulk recall we should run a Spectrum Archive node, so we check if this is the active control node
+  singleton="archive";;
 
 "test")
   # just a simple test using ls
@@ -207,7 +209,7 @@ logF=$logDir"/"$op"_"$curDate".log"
 # logF=$logDir"/"debug.log
 
 #present banner and initialize log file
-echo "$(date) LAUNCH: launcher version $ver started with operation $op $secArg on node $(hostname)" > $logF
+echo "$(date) LAUNCH: launcher version $ver started operation $op $fsName $secArg on node $localNode" > $logF
 echo >> $logF
 
 # check if events are enabled and set sendEvent accordingly
@@ -228,36 +230,45 @@ then
 fi
 echo "LAUNCH: send Event is set to $sendEvent" >> $logF
 
- 
 #if there was an error before write it to the log and exit
 if [[ ! -z $errMsg ]];
 then
   echo -e $errMsg >> $logF
-  if [[ -z $2 ]]; then fsName=UNKNOWN; fi
-  execNode=$($gpfsPath/mmlsnode -N localhost | cut -d'.' -f1)
+  if [[ -z $fsName ]]; then fsName=UNKNOWN; fi
   if [[ -z $op ]]; then op=UNKNOWN; fi
+  if [[ -z $localNode ]]; then localNode=UNKNOWN; fi  
   if (( sendEvent == 1 )); 
   then 
-    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$execNode,$logF" >> $logF 2>&1
+    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$localNode,$logF" >> $logF 2>&1
   fi
   exit $errCode
 fi
 
-
-# check if command scripts executing the storage operation exist on this node
-if [[ ! -a $cmd ]];
+#check that localNode initialized above is not empty
+if [[ -z $localNode ]];
 then
-  echo "LAUNCH: ERROR command $cmd does not exists. Make sure that the script path is set and the scripts are placed in this path (current script path: $scriptPath, check script: $checkScript" >> $logF
+  echo "LAUNCH: ERROR local node name could not be initialized, node may be down." >> $logF
   if (( sendEvent == 1 )); 
   then 
-    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$execNode,$logF" >> $logF 2>&1
+    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,"UNKNOWN",$logF" >> $logF 2>&1
   fi
   exit $rcErr
 fi
-  
 
-#assign and check file system name. If $2 is set then use this, if not use the default name (def_fsName). If this is empty then terminate
-echo "LAUNCH: assigning and checking file system name" >> $logF
+# check if command scripts executing the storage operation exist on this node
+scriptName=$(echo $cmd | awk '{print $1}')
+if [[ ! -a $scriptName ]];
+then
+  echo "LAUNCH: ERROR script $scriptName does not exists. Make sure that the script path is set and the scripts are placed in this path (current script path: $scriptPath)" >> $logF
+  if (( sendEvent == 1 )); 
+  then 
+    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$localNode,$logF" >> $logF 2>&1
+  fi
+  exit $rcErr
+fi
+
+#check and assign file system name. If $2 is set then use this, if not use the default name (def_fsName). If this is empty then terminate
+echo "LAUNCH: assigning and checking file system name ($fsName)" >> $logF
 if [[ -z $fsName ]];
 then 
   if [[ ! -z $def_fsName ]];
@@ -267,7 +278,7 @@ then
     echo "LAUNCH: ERROR file system name not defined. Either pass it as parameter to this script. Or initialized the global variable fsName" >> $logF
     if (( sendEvent == 1 )); 
     then 
-      $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$execNode,$logF" >> $logF 2>&1
+      $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$localNode,$logF" >> $logF 2>&1
     fi
     exit $rcErr
   fi
@@ -281,36 +292,12 @@ then
   echo "LAUNCH: ERROR file system $fsName does not exist." >> $logF
   if (( sendEvent == 1 )); 
   then 
-    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$execNode,$logF" >> $logF 2>&1
+    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$localNode,$logF" >> $logF 2>&1
   fi
   exit $rcErr
 fi
 # now export the parameter $fsName which could be used by scripts being launched
 export fsName=$fsName
-
-
-# compose the command (operation) to run
-# if we run the check operation do not include the file system name
-if [[ "$op" == "check" ]];
-then 
-  cmd=$cmd" $secArg"
-else 
-  cmd=$cmd" $fsName $secArg"
-fi
-
-#check that localNode initialized above is not empty
-#perform this check prior deleting the log files to keep the previous one.
-localNode=$($gpfsPath/mmlsnode -N localhost | cut -d'.' -f1)
-if [[ -z $localNode ]];
-then
-  echo "LAUNCH: ERROR local node name could not be initialized, node may be down." >> $logF
-  if (( sendEvent == 1 )); 
-  then 
-    $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$execNode,$logF" >> $logF 2>&1
-  fi
-  exit $rcErr
-fi
-
 
 # delete and compress older  logfiles prior to logging anything
 echo "LAUNCH: cleaning up log files  $op" >> $logF
@@ -332,14 +319,57 @@ do
 done
 
 
-#check if node is cluster manager
-echo "LAUNCH: checking if this node is cluster manager" >> $logF
-clusterMgr=$($gpfsPath/mmlsmgr -c | sed 's|.*(||' | sed 's|)||')
-if [ "$localNode" != "$clusterMgr" ]; 
+# if singleton is manager we check if we run on a manager node, if singleton is archive we check if we run on a archive node
+if [[ "$singleton" == "archive" ]]; 
 then
-  echo "LAUNCH: INFO this node is not cluster manager, exiting." >> $logF
-  echo "LAUNCH: DEBUG localNode=$localNode  clusterMgr=$clusterMgr" >> $logF
-  exit $rcGood
+  if [[ ! -a $eeCmd  || ! -a $JQ_TOOL ]]; 
+  then
+    echo "LAUNCH: ERROR: required tool $eeCmd or $JQ_TOOL does not exist on this node. Cannot check if this node is active control node. Exiting." >> $logF
+	if (( sendEvent == 1 )); 
+    then 
+      $gpfsPath/mmsysmonc event custom $eventErr "$fsName,$op,$localNode,$logF" >> $logF 2>&1
+    fi
+    exit $rcErr	
+  fi
+  found=0
+  out=""
+  #check if node is active control node 
+  echo "LAUNCH: checking if this node is active control node." >> $logF
+  out=$($eeCmd node list --json |  $JQ_TOOL -r '.payload[] | [.hostname, .active_control_node] | @csv' 2>&1)
+  rc=$?
+  if [[ ! -z $out && $rc == 0 ]] ; then
+    while read line ; do
+      hn=$(echo $line | cut -d',' -f1 | cut -d'"' -f2)	# hostname
+      ac=$(echo $line | cut -d',' -f2)					# active control node ?
+
+      # localNode comes from mmlsnode and has node name with dots: ltfs1.ltfs.net
+	  # $hn comes from eeadm drive list and has dots: ltfs1.ltfs.net
+      if [[ $localNode == $hn && $ac == "true" ]] ; then
+        found=1
+        break
+      fi
+    done <<< "$(echo -e "$out")"
+  else 
+    echo "LAUNCH: WARNING unable to determine if this is active control node. Assuming it is not." >> $logF
+	echo "LAUNCH: DEBUG out string from eeadm node list: $out, return code = $rc." >> $logF
+  fi
+  if (( $found == 0 )) ; then
+    echo "LAUNCH: INFO this node is not active control node, exiting." >> $logF
+    echo "LAUNCH: DEBUG localNode=$localNode  activeNode=$hn" >> $logF
+    exit $rcGood
+  fi
+else
+  #check if node is cluster manager
+  echo "LAUNCH: checking if this node is cluster manager" >> $logF
+  # have to cut of the dots from local node because mmlsmgr shows node name without dots: Cluster manager node: 192.168.100.2 (ltfs2)
+  localNodeWithoutDot=$(echo "$localNode" | cut -d'.' -f 1)
+  clusterMgr=$($gpfsPath/mmlsmgr -c | sed 's|.*(||' | sed 's|)||')
+  if [ "$localNodeWithoutDot" != "$clusterMgr" ]; 
+  then
+    echo "LAUNCH: INFO this node is not cluster manager, exiting." >> $logF
+    echo "LAUNCH: DEBUG localNode=$localNodeWithoutDot  clusterMgr=$clusterMgr" >> $logF
+    exit $rcGood
+  fi
 fi
 
 #-------------------------------
@@ -350,6 +380,7 @@ sortNodes=""
 # if node class is set up determine node names in node class
 if [[ ! -z $nodeClass ]];
 then
+   # mmlsnodeclass shows nodes with dots: ltfs2.ltfs.net,ltfs1.ltfs.net
    echo "LAUNCH: INFO node class to select the node from is: $nodeClass" >> $logF
    allNodes=$($gpfsPath/mmlsnodeclass $nodeClass -Y | grep -v HEADER | cut -d':' -f 10 | sed 's|,| |g')
    if [[ -z $allNodes ]]
@@ -376,16 +407,22 @@ fi
 # select the node to execute the command based on state 
 echo "LAUNCH: INFO The following nodes are checked to run the operation: $sortNodes" >> $logF
 execNode=""
+# determine file system state on node
+# mmlsmount shows nodes without dots: mmlsmount::0:1:::ltfscache:ltfscache:ltfs.net:2:192.168.100.2:ltfs2:ltfs.net:RW:
+mNodes=$($gpfsPath/mmlsmount $fsName -Y | grep -v HEADER | grep -E ":RW:" | cut -d':' -f 12)
 for n in $sortNodes;
 do
   # determine node state
+  # mmgetstate shows node with dots: mmgetstate::0:1:::ltfs1.ltfs.net:1:active:1*:2:2:quorum node:(undefined):
   state=$($gpfsPath/mmgetstate -N $n -Y | grep -v ":HEADER:" | cut -d':' -f 9)
   if [[ "$state" == "active" ]];
   then
-	# determine file system state on node
-	mNodes=$($gpfsPath/mmlsmount $fsName -Y | grep -v HEADER | grep -E ":RW:" | cut -d':' -f 12)
 	for m in $mNodes;
 	do
+	  # $n comes from mmlsnodeclass where node name is with dot: ltfs2.ltfs.net
+	  # $m comes from mmlsmount where node name is without dots: ltfs2
+	  # have to remove the dot from $n
+	  n=$(echo "$n" | cut -d'.' -f1)
 	  if [[ "$m" == "$n" ]];
 	  then
 		execNode=$m
