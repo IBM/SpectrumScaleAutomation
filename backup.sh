88940ab862604a7cd8db827813449bbe234b77f5
N. Haustein <haustein@de.ibm.com>
2020-08-15 16:38:55 +0200

    Merge NYU improvements: for fileset level backup check if shadow DB exists and if not run -t full, improve messaging upon completion

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 ################################################################################
 # The MIT License (MIT)                                                        #
 #                                                                              #
-# Copyright (c) 2019 Nils Haustein                             				   #
+# Copyright (c) 2020 Nils Haustein                             				   #
 #                                                                              #
 # Permission is hereby granted, free of charge, to any person obtaining a copy #
 # of this software and associated documentation files (the "Software"), to deal#
@@ -31,21 +31,28 @@
 # - perform mmbackup 
 # - delete snapshot when required
 #
-# Invokation: backup.sh file-system-name [fileset-name]
+# Invokation: backup.sh file-system-name [fileset-name] [backup type]
 #             file-system-name is the name of the file system to be backed up
 #             file-set-name (optional) name of the independent fileset to backup from
+#			  backup-type (optional) can be full or incremental (incremental is default)
+#
+#             Note, the order of the command line parameters matters
 #
 # Output: all output is logged to STDOUT
 #
 # Author: Nils Haustein
 #
-# 
+# Version: 1.2
+#
 # Change History
 # --------------
 # 2017: first implementation for client project
 # 10/18/19: added license for github
 # 11/14/19: add GPFS path for GPFS commands
-# 11/15/19; added the option to backup from fileset, fileset name is given with the call
+# 11/15/19: added the option to backup from fileset, fileset name is given with the call
+# 06/10/20: add config parameter $weekday defining when to run mmbackup -q (version 1.1)
+# 06/11/20: NYU: for fileset level backup check if .mmbackupShadow* exists in the path and if not then run full backup
+# 08/14/20: streamlined return message, added hint to rebuild shadow DB if mmbackup rc > 1 (version 1.2)
 ##################################################################################
 
 #
@@ -55,14 +62,21 @@
 # name of TSM server to be used with mmbackup, if not set then we used the default server
 tsmServ=""
 
+# mmbackup parameters to be used with mmbackup, adjust if necessary
+backupOpts="-N g8_node3 -v --max-backup-count 4096 --max-backup-size 80M --backup-threads 2 --expire-threads 2"
+
 # name of global snapshot for mmbackup, if this is not set then mmbackup will not backup from snapshot
 snapName=""
 
-# mmbackup parameters to be used with mmbackup, adjust if necessary
-backupOpts="-N nsdNodes -v --max-backup-count 4096 --max-backup-size 80M --backup-threads 2 --expire-threads 2"
+# specify the day of the week when to run mmbackup -q
+# 0=skip, 1=monday, 2=tuesday, ... 7=sunday
+weekDay=0
 
 # Constants
 # ----------
+# version of the program
+ver=1.2
+
 # specifies the path for the GPFS binaries
 gpfsPath="/usr/lpp/mmfs/bin"
 
@@ -71,13 +85,18 @@ rcGood=0  # successful run
 rcWarn=1  # run was ok, some warnings however
 rcErr=2   # failed
 
-# default file system name
+# Assign command line parameter
+# ------------------------------
+# assign file system name
 fsName="$1"
+
+# assign file set name
 fsetName="$2"
+
 
 #*************************** Main ***************************
 # present banner
-echo "$(date) BACKUP: backup operation for file system $fsName started on $(hostname)."
+echo "$(date) BACKUP: backup operation version $ver for file system $fsName started on $(hostname)."
 
 # check if file system is initialized
 if [[ -z $fsName ]];
@@ -148,12 +167,16 @@ echo "$(date) BACKUP: Starting mmbackup for file system $fsName"
 # export MMBACKUP_IGNORE_INCLUDE=1
 #run mmbackup with -q every other day when scheduled before 12:00 noon
 
-# perform shadow DB sync every Sunday
+# perform shadow DB sync on the weekday specified: 1=monday, 2=tuesday, ... 7=sunday
+# if weekDay=0 then skip query
 rebuild=""
-d=$(date +%u)
-if (( d == 7 )); 
-then
-  rebuild="-q"
+if (( weekDay > 0 ));
+then 
+  d=$(date +%u)
+  if (( d == $weekDay )); 
+  then
+    rebuild="-q"
+  fi
 fi
 
 # assign mmbackup options (snapshot when required, tsmserver, rebuild)
@@ -174,27 +197,28 @@ if [[ ! -z $fsetName ]];
 then
    fsDev=$fsetPath
    bOpts="$bOpts --scope inodespace"
+   
+   # the first backup of a fileset must be full, therefore check if the shadow DB exists and if not then run -t full
+   fName="/ibm/auditfs/test/.mmbackupshadow*"
+   ls $fsetPath/.mmbackupShadow* > /dev/null 2>&1
+   rc=$?
+   if (( rc != 0 ));
+   then
+     bOpts="$bOpts -t full"
+   fi
 else
    fsDev=$fsName
 fi
 
-echo "BACKUP: DEBUG mmbackup $fsDev $bOpts $backupOpts"
+
+echo "BACKUP: DEBUG $gpfsPath/mmbackup $fsDev $bOpts $backupOpts"
 mmbackup_rc=0
 $gpfsPath/mmbackup $fsDev $bOpts $backupOpts
 mmbackup_rc=$?
-#examine mmbackup return code: 0 good, 1 warning, 2 error
-if (( mmbackup_rc > 0 ));
-then
-  if (( mmbackup_rc > 1 ));
-  then 
-    echo "BACKUP: ERROR mmbackup for $fsDev failed with rc=$mmbackup_rc."
-    mmbackup_rc=2
-  else
-    echo "BACKUP: WARNING mmbackup for $fsDev ended with rc=$mmbackup_rc."
-  fi
-fi
+
 
 #delete snapshot when required
+rc=0
 if [[ ! -z $snapName ]];
 then
   if [[ ! -z $fsetName ]];
@@ -216,13 +240,19 @@ fi
 # evaluate return codes and exit according to defined return codes
 if (( mmbackup_rc > 1 ));
 then 
-  echo "$(date) BACKUP: operation for file system $fsName ended with ERRORS"
+  echo "$(date) BACKUP: ERROR running backup for $fsDev (rc=$mmbackup_rc)"
+  echo "BACKUP HINT: investigate the log file. Consider resynchronizing or rebuilding the shadow data base (mmbackup options -q or --rebuild)."
   exit $rcErr
 elif (( mmbackup_rc == 1 || rc > 0 ));
 then 
-  echo "$(date) BACKUP: operation for file system $fsName ended with WARNINGS"
+  echo "$(date) BACKUP: WARNING running backup for $fsDev. "
+  if (( mmbackup_rc == 1 )); then
+    echo "BACKUP HINT: investigate the mmbackup log file and the SP server log. Run backup in debug mode (mmbackup option -L)."
+  elif (( rc > 0 )); then
+    echo "BACKUP HINT: delete the snapshot and try again."
+  fi	
   exit $rcWarn
 else
-  echo "$(date) BACKUP: operation for file system $fsName ended with SUCCESS"
+  echo "$(date) BACKUP: SUCESS running backup for $fsDev."
   exit $rcGood
 fi
